f1e4b0627e943ca9b1fce567af8075d67075b519
laluhawari <laluhusnihawari@gmail.com>
2021-05-11 14:38:30 +0800

    App Kalkulator sederhana

---
+++ Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtAngka1 = New System.Windows.Forms.TextBox()
+        Me.txtAngka2 = New System.Windows.Forms.TextBox()
+        Me.BtnTambah = New System.Windows.Forms.Button()
+        Me.BtnKurang = New System.Windows.Forms.Button()
+        Me.BtnBagi = New System.Windows.Forms.Button()
+        Me.BtnKali = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtHasil = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(74, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(276, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "KALKULATOR SEDERHANA"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(59, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Angka Pertama"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(59, 143)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Angka Kedua"
+        '
+        'txtAngka1
+        '
+        Me.txtAngka1.Location = New System.Drawing.Point(159, 102)
+        Me.txtAngka1.Name = "txtAngka1"
+        Me.txtAngka1.Size = New System.Drawing.Size(100, 20)
+        Me.txtAngka1.TabIndex = 3
+        '
+        'txtAngka2
+        '
+        Me.txtAngka2.Location = New System.Drawing.Point(159, 143)
+        Me.txtAngka2.Name = "txtAngka2"
+        Me.txtAngka2.Size = New System.Drawing.Size(100, 20)
+        Me.txtAngka2.TabIndex = 4
+        '
+        'BtnTambah
+        '
+        Me.BtnTambah.Location = New System.Drawing.Point(47, 226)
+        Me.BtnTambah.Name = "BtnTambah"
+        Me.BtnTambah.Size = New System.Drawing.Size(44, 23)
+        Me.BtnTambah.TabIndex = 5
+        Me.BtnTambah.Text = "+"
+        Me.BtnTambah.UseVisualStyleBackColor = True
+        '
+        'BtnKurang
+        '
+        Me.BtnKurang.Location = New System.Drawing.Point(128, 226)
+        Me.BtnKurang.Name = "BtnKurang"
+        Me.BtnKurang.Size = New System.Drawing.Size(43, 23)
+        Me.BtnKurang.TabIndex = 6
+        Me.BtnKurang.Text = "-"
+        Me.BtnKurang.UseVisualStyleBackColor = True
+        '
+        'BtnBagi
+        '
+        Me.BtnBagi.Location = New System.Drawing.Point(210, 226)
+        Me.BtnBagi.Name = "BtnBagi"
+        Me.BtnBagi.Size = New System.Drawing.Size(49, 23)
+        Me.BtnBagi.TabIndex = 7
+        Me.BtnBagi.Text = "/"
+        Me.BtnBagi.UseVisualStyleBackColor = True
+        '
+        'BtnKali
+        '
+        Me.BtnKali.Location = New System.Drawing.Point(288, 226)
+        Me.BtnKali.Name = "BtnKali"
+        Me.BtnKali.Size = New System.Drawing.Size(51, 23)
+        Me.BtnKali.TabIndex = 8
+        Me.BtnKali.Text = "x"
+        Me.BtnKali.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(101, 303)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(30, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Hasil"
+        '
+        'txtHasil
+        '
+        Me.txtHasil.Location = New System.Drawing.Point(148, 300)
+        Me.txtHasil.Name = "txtHasil"
+        Me.txtHasil.Size = New System.Drawing.Size(100, 20)
+        Me.txtHasil.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(413, 367)
+        Me.Controls.Add(Me.txtHasil)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.BtnKali)
+        Me.Controls.Add(Me.BtnBagi)
+        Me.Controls.Add(Me.BtnKurang)
+        Me.Controls.Add(Me.BtnTambah)
+        Me.Controls.Add(Me.txtAngka2)
+        Me.Controls.Add(Me.txtAngka1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Kalkulator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtAngka1 As TextBox
+    Friend WithEvents txtAngka2 As TextBox
+    Friend WithEvents BtnTambah As Button
+    Friend WithEvents BtnKurang As Button
+    Friend WithEvents BtnBagi As Button
+    Friend WithEvents BtnKali As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtHasil As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Dim Angka1, Angka2, Hasil As Double
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub BtnTambah_Click(sender As Object, e As EventArgs) Handles BtnTambah.Click
+        Angka1 = Val(txtAngka1.Text)
+        Angka2 = Val(txtAngka2.Text)
+        Hasil = Angka1 + Angka2
+        txtHasil.Text = Hasil
+    End Sub
+
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles BtnBagi.Click
+        Angka1 = Val(txtAngka1.Text)
+        Angka2 = Val(txtAngka2.Text)
+        Hasil = Angka1 / Angka2
+        txtHasil.Text = Hasil
+    End Sub
+
+    Private Sub TxtAngka1_TextChanged(sender As Object, e As EventArgs) Handles txtAngka1.TextChanged
+
+    End Sub
+
+    Private Sub BtnKurang_Click(sender As Object, e As EventArgs) Handles BtnKurang.Click
+        Angka1 = Val(txtAngka1.Text)
+        Angka2 = Val(txtAngka2.Text)
+        Hasil = Angka1 - Angka2
+        txtHasil.Text = Hasil
+    End Sub
+
+    Private Sub BtnKali_Click(sender As Object, e As EventArgs) Handles BtnKali.Click
+        Angka1 = Val(txtAngka1.Text)
+        Angka2 = Val(txtAngka2.Text)
+        Hasil = Angka1 * Angka2
+        txtHasil.Text = Hasil
+    End Sub
+End Class
